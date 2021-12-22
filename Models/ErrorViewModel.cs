2d65104e5d009ec050cb8b6e64a4354c76e97e6c
Lucas Silva <lucas.silva@I2S.pt>
2021-12-22 21:07:12 +0000

    Project created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_core_identity_basics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
